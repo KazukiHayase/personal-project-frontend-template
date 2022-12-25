81ca044f8cdc58e36188439380ac64378be9d160
KazukiHayase <k.hayase@buysell-technologies.com>
2022-12-25 20:06:52 +0900

    customScalars修正

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ module.exports = {
       schema: './schema.graphql',
       exclude: ['**/node_modules/**', '**/__mocks__/**', '**/__generated__/**'],
       language: 'typescript',
-      customScalars: {Cursor: 'string'}
+      customScalars: {}
     },
   },
 }
--- relay.config.js
+++ relay.config.js
@@ -3,5 +3,5 @@ module.exports = {
   schema: './schema.graphql',
   exclude: ['**/node_modules/**', '**/__mocks__/**', '**/__generated__/**'],
   language: 'typescript',
-  customScalars: {Cursor: 'string'}
+  customScalars: {}
 }
