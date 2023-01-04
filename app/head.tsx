33deb16371f40e0b132ef3de36af3e0ea1840c1c
KazukiHayase <k.hayase@buysell-technologies.com>
2023-01-04 16:11:04 +0900

    head追加

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+// TODO: title, description, favicon
+export default function Head() {
+  return (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <title>title</title>
+      <meta name="description" content="description" />
+    </>
+  );
+}
