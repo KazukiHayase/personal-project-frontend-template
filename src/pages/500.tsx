cdb39cc30a411b30dbed8fc182334df398f72baa
KazukiHayase <k.hayase@buysell-technologies.com>
2023-01-04 16:24:11 +0900

    pages削除

--- src/pages/500.tsx
+++
@@ -1,8 +0,0 @@
-import { NextPage } from 'next';
-import { Error } from '@/components/Error';
-
-const IndexPage: NextPage = () => {
-  return <Error />;
-};
-
-export default IndexPage;
--- src/pages/index.tsx
+++
@@ -1,7 +0,0 @@
-import { NextPage } from 'next';
-
-const IndexPage: NextPage = () => {
-  return <h1>IndexPage</h1>;
-};
-
-export default IndexPage;
